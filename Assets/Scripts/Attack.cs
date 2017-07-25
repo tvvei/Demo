c7628391378f87cca49f69aad3a8230c6ad9e3b0
chaolun <0chaolun0@gmail.com>
2017-07-26 00:29:28 +0800

    - update health

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -2,6 +2,8 @@
 
 public class Attack : MonoBehaviour
 {
+	public float damage = 30;
+
 	void Start ()
 	{
 		
@@ -12,6 +14,10 @@ public class Attack : MonoBehaviour
 		if (collider.tag == "prefab") {
 			MeshRenderer mr = collider.gameObject.GetComponent<MeshRenderer> ();
 			mr.material.color = new Color (Random.Range (0, 1f), Random.Range (0, 1f), Random.Range (0, 1f), 0.5f);
+			Health health = collider.GetComponent<Health> ();
+			if (health != null) {
+				health.Damage (damage);
+			}
 		}
 	}
 }
